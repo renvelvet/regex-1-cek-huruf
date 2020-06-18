c7523720d90ebc8b3ed06be8bcf984f62a076623
Resha Puspita <reshapuspita734@gmail.com>
2020-06-18 11:08:49 +0700

    debug solved

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const buttonCekHuruf = document.querySelector(".cek-huruf .buttonCek");
 // ------------ FUNCTION -------------------
 cekHurufAwal = (string) => {
-  const regEx = new RegExp(/[A-Z]\w/);
+  const regEx = new RegExp(/[A-Z]/);
   return regEx.test(string);
 };
 
 // ----------- EVENT LISTENER ------------------
 buttonCekHuruf.addEventListener("click", () => {
   const cekHuruf = document.getElementById("huruf").value;
-  if (cekHurufAwal(cekHuruf)) {
+  if (cekHurufAwal(cekHuruf.charAt(0))) {
     document.querySelector(".cek-huruf #output").innerHTML =
       "Huruf pertama adalah huruf besar";
   } else {
